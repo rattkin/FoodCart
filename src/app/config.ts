791940b0bb1d43fda85f18b88c3addd5b0919589
Jan Müller <muller.jan@gmail.com>
2020-06-07 12:33:57 +0200

    testing preset

--- src/app/config.ts
+++ src/app/config.ts
@@ -11,8 +11,7 @@ export const timeToPrepareOrder = 15; // minutes
 export const roundingFactor = 5; // minutes
 
 export const startTime = moment('10:30', timeFormat);
-// Testing
-// export const endTime = moment('23:59', timeFormat);
+// export const endTime = moment('23:59', timeFormat); // Testing
 export const endTime = moment('20:00', timeFormat);
 export const startDay = moment().isoWeekday(1);
 export const endDay = moment().isoWeekday(6);
@@ -20,6 +19,6 @@ export const endDay = moment().isoWeekday(6);
 
 export const startMenuTime = moment('10:30', timeFormat);
 export const endMenuTime = moment('15:00', timeFormat);
-// export const endMenuTime = moment('23:59', timeFormat);
+// export const endMenuTime = moment('23:59', timeFormat); // Testing
 export const startMenuDay = moment().isoWeekday(1);
 export const endMenuDay = moment().isoWeekday(5);
