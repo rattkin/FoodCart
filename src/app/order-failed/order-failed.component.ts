35775170ad8855caf9fe9bb3cb1b8297107397b0
Jan Müller <muller.jan@gmail.com>
2020-06-15 22:03:05 +0200

    clean up dialogs

--- src/app/order-failed/order-failed.component.ts
+++ src/app/order-failed/order-failed.component.ts
@@ -1,16 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { Store } from '@ngrx/store';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-order-failed',
   templateUrl: './order-failed.component.html',
   styleUrls: ['./order-failed.component.css']
 })
-export class OrderFailedComponent implements OnInit {
-
-  constructor(private store: Store<any>) { }
-
-  ngOnInit(): void {
-  }
-
+export class OrderFailedComponent {
 }
--- src/app/order-successful/order-successful.component.ts
+++ src/app/order-successful/order-successful.component.ts
@@ -1,16 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { Store } from '@ngrx/store';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-order-successful',
   templateUrl: './order-successful.component.html',
   styleUrls: ['./order-successful.component.css']
 })
-export class OrderSuccessfulComponent implements OnInit {
 
-  constructor(private store: Store<any>) { }
-
-  ngOnInit(): void {
-  }
-
+export class OrderSuccessfulComponent {
 }
