d5923de85d70c5c1536be207a55d733c5d2b6351
Jan Müller <muller.jan@gmail.com>
2020-06-15 22:57:05 +0200

    remove unused

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,24 +8,23 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { NgxMaterialTimepickerModule } from 'ngx-material-timepicker';
 import { environment } from 'src/environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppEffects } from './app.effects';
-import { MaterialModule } from './material-module';
+import { AreYouSureComponent } from './are-you-sure/are-you-sure.component';
+import { HomePageComponent } from './home-page/home-page.component';
 import { ItemListComponent } from './item-list/item-list.component';
+import { MaterialModule } from './material-module';
+import { OrderListComponent } from './order-button/order-button.component';
 import { OrderDialogComponent } from './order-dialog/order-dialog.component';
 import { OrderFailedComponent } from './order-failed/order-failed.component';
-import { OrderListComponent } from './order-button/order-button.component';
 import { OrderSuccessfulComponent } from './order-successful/order-successful.component';
-import { PickSideDishComponent } from './pick-side-dish/pick-side-dish.component';
-import * as fromReducers from './state/reducers/mealStore';
-import { TopMenuComponent } from './top-menu/top-menu.component';
-import { SideMenuComponent } from './side-menu/side-menu.component';
-import { NgxMaterialTimepickerModule } from 'ngx-material-timepicker';
 import { PickHeatComponent } from './pick-heat/pick-heat.component';
-import { HomePageComponent } from './home-page/home-page.component';
-import { AreYouSureComponent } from './are-you-sure/are-you-sure.component';
+import { PickSideDishComponent } from './pick-side-dish/pick-side-dish.component';
+import { SideMenuComponent } from './side-menu/side-menu.component';
+import * as fromReducers from './state/reducers/mealStore';
 
 @NgModule({
   declarations: [
@@ -36,7 +35,6 @@ import { AreYouSureComponent } from './are-you-sure/are-you-sure.component';
     OrderDialogComponent,
     OrderSuccessfulComponent,
     OrderFailedComponent,
-    TopMenuComponent,
     SideMenuComponent,
     PickHeatComponent,
     HomePageComponent,
